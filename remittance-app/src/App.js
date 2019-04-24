a583a2b23d207d3def6f2b8ff4acee5d7d0f9590
ARusani <alessandro.rusani@gmail.com>
2019-04-25 01:11:07 +0200

    Solved problem with web3.currentProvider and truffle-contract

--- remittance-app/src/App.js
+++ remittance-app/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { default as contract } from 'truffle-contract';
+import truffleContract from 'truffle-contract';
 import remittanceJson from '../../node_modules/remittance-contracts/build/contracts/Remittance.json';
 import Web3 from 'web3';
 
@@ -18,28 +18,32 @@ class App extends React.Component {
   componentDidMount = async () => {
     try {
       // Get network provider and web3 instance.
-      const web3 = await this.injectWeb3();
+      const web3 = new Web3(Web3.givenProvider || new Web3.providers.HttpProvider('http://localhost:9545'));
       console.log(web3)
 
+      web3.eth.getTransactionReceiptMined = require('../../node_modules/remittance-contracts/gistLepretre/getTransactionReceiptMined.js');
+      
       // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
       console.log('accounts[0]=',accounts[0]);
-      this.setState({ account: accounts[0] });
 
-      const remittanceContract = contract(remittanceJson);
+      const remittanceContract = truffleContract(remittanceJson);
       remittanceContract.setProvider(web3.currentProvider);
-      if (typeof remittanceContract.currentProvider.sendAsync !== "function") {
-/*         remittanceContract.currentProvider.sendAsync = () => {
+     
+       if (typeof remittanceContract.currentProvider.sendAsync !== "function") {
+        remittanceContract.currentProvider.sendAsync = function() {
             return remittanceContract.currentProvider.send.apply(
-              remittanceContract.currentProvide
+              remittanceContract.currentProvider, arguments
             );
-        }; */
-      }
+        };
+      }; 
+
       const instance = await remittanceContract.deployed();
+      console.log(instance);
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3: web3, contract: instance });
+      this.setState({ web3: web3, accounts: accounts, contract: instance });
       this.addEventListener(this);
     } catch (error) {
       // Catch any errors for any of the above operations.
@@ -50,58 +54,24 @@ class App extends React.Component {
     }
   };
 
-  injectWeb3 = async () => {
-    return new Promise((resolve, reject) => {
-      window.addEventListener("load", async () => {
-        // Modern dapp browsers...
-        if (window.ethereum) {
-          const web3 = new Web3(window.ethereum);
-          try {
-            // Request account access if needed
-            await window.ethereum.enable();
-            resolve(web3);
-          } catch (error) {
-            // User denied account access...
-            console.log(error);
-            reject(error);
-          }
-        }
-        // Legacy dapp browsers...
-        else if (window.web3) {
-          // Use Mist/MetaMask's provider.
-          const web3 = window.web3;
-          console.log("Injected web3 detected.");
-          resolve(web3);
-        }
-        // Fallback to localhost;
-        else {
-          const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:9545')); 
-          console.log("No web3 instance injected, using Local web3.");
-          resolve(web3);
-        }
-      });
-    });
-  };
+  otp = async (event) => {
+     try {
+        const { web3, accounts, contract } = this.state;
+        const {toWei, sha3} = web3.utils;
 
-  depositFund = async(val) => {
-/*     try {
-      const { accounts, contract } = this.state;
-      const result = await contract.set(val, { from: accounts[0] });
-      console.log("set receipt status", result.receipt.status);
-      if (parseInt(result.receipt.status) !== 1) {
-        throw new Error("Failed to set value");
+        const result = await contract.otp(sha3(event.target.Password1.value), sha3(event.target.Password2.value), toWei(event.target.etherAmount.value), { from: accounts[0] });
+        console.log("set receipt status", result.receipt.status);
+        if (parseInt(result.receipt.status) !== 1) {
+          throw new Error("Failed to set value");
       }
-      // this.setState({ storageValue: result.logs[0].args.value.toString(10)});
-      // We skip using the event here because we add a listener.
     } catch(error) {
-      alert(`Failed to set value to $val`);
       console.log(error);
-    } */
+    } 
   };
 
-  addEventListener(component) {
-    const updateEvent = this.state.contract.EventDepositFund({}, { fromBlock: 0, toBlock: 'latest' });
-    updateEvent.watch(function(err, result) {
+  addEventListener = async (component) => {
+    const eventDepositFund = this.state.contract.EventDepositFund({}, { fromBlock: 0, toBlock: 'latest' });
+    eventDepositFund.watch(function(err, result) {
       if (err) {
         console.log(err);
         return;
@@ -120,28 +90,15 @@ class App extends React.Component {
         <h1 style={{ fontSize: '32px', marginBottom: '20px' }}>Remittance</h1>
         <span id="insTrans"></span>
         <h2 style={{ fontSize: '20px', marginBottom: '20px' }}>OTP</h2>
-          <form>
-            <label>
-              Password1:
-              <input type="text" name="Password1" />
-            </label>
-            <label>
-              Password2:
-              <input type="text" name="Password2" />
-            </label>
-          </form>
-          <h2 style={{ fontSize: '20px', marginBottom: '20px' }}>Deposit</h2>                      
-          <form>
-            <label>
-              Password:
-              <input type="text" name="Password" />
-            </label>
-            <label>
-              Ether to send: 
-              <input type="text" name="etherAmount" />
-            </label>
-            <input type="Deposit" value="Submit" onClick={ () => this.depositFund(2) }/>
-          </form>
+        <form onSubmit={this.otp.bind(this)}>
+          <label>Password1:</label>
+          <input type="text" name="Password1" />
+          <label>Password2:</label>
+          <input type="text" name="Password2" />
+          <label>Ethers Amount:</label>
+          <input type="text" name="etherAmount" />
+          <button type="submit">Generate</button>
+        </form>  
         </header>
       </div>
     )
